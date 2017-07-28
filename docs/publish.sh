09a226d136fc0de628f001e763a6923588eaac33
Mu Li <muli@cs.cmu.edu>
2017-07-27 22:37:47 -0700

    Update publish.sh

--- docs/publish.sh
+++ docs/publish.sh
@@ -7,14 +7,12 @@ set -e
 NOTEBOOK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 cd ${NOTEBOOK_DIR}
 
+# install a gpu version
+sed -i.bak s/mxnet/mxnet-cu80/g environment.yml 
+
 # prepare the env
 conda env update -f environment.yml 
 source activate gluon_docs
-
-# install a gpu version
-pip uninstall mxnet
-pip install --pre mxnet-cu80
-pip show mxnet
 
 make html
 
