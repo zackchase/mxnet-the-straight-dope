daa3ed6d274a4e7beac09c294ca65c3f871353ca
Vishaal Kapoor <vkapoor3141@gmail.com>
2018-10-11 14:34:24 -0700

    Create new conda environment to get latest packages.
    The current conda environment is stuck on old versions and those old
    versions are failing the build. Recreating Conda environment to allow
    for latest versions to be chosen.

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,8 @@ set -x
 set -e
 
 # prepare the env
-conda env update -f build/build.yml
-conda activate build_gluon_tutorials
+conda env update -f build/build.yml -n build_sd_tutorials
+conda activate build_sd_tutorials
 
 make html
 
